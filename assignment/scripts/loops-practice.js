d7a1d870f2918ccdf6c1051596894c84f8614fb4
Kelsey Peterson <kelsey.peterson14@gmail.com>
2021-12-19 13:06:28 -0600

    Cleaned up syntax

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -47,7 +47,7 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 for (let i=0; i<stars.length; i++) {
-  console.log(stars [i]);
+  console.log(stars[i]);
 }
 
 // 3. While loops
